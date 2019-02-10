92ddd1a54100c94beee8f9f19b1e79fa8801fc67
blukasho <bodik1w@gmail.com>
2019-02-10 11:54:18 +0200

    parse done

--- srcs/ft_printf/main.c
+++ srcs/ft_printf/main.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/24 12:03:03 by blukasho          #+#    #+#             */
-/*   Updated: 2019/02/09 18:36:49 by blukasho         ###   ########.fr       */
+/*   Updated: 2019/02/09 19:35:09 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,6 @@ int		main(void)
 {
 //	ft_putnbr(ft_printf("Hello,%#-+ 0123456.10llcWorld!!!\n", 7));
 //	ft_putendl("");
-	printf("Hello, %-10.5d Worlds!!!\n", 733);
+	printf("Hello, %-10.5d World!!!\n", 733);
 	return (0);
 }
