81f7e1c35568fe5c433057ba6ec4d4ed6c9c31e5
blukasho <bodik1w@gmail.com>
2019-03-30 23:37:58 +0200

    full fix round double

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: blukasho <bodik1w@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/30 10:23:32 by blukasho          #+#    #+#             */
-/*   Updated: 2019/03/30 19:22:31 by blukasho         ###   ########.fr       */
+/*   Updated: 2019/03/30 23:36:27 by blukasho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ int					main(void)
 	double			ld;
 	char			*s;
 
-	ld = 6.5;
+	ld = 0.5;
 
-	s = "|%.100F|\n";
+	s = "|%.F|\n";
 
 	my = ft_printf(s, ld);
     sy = printf(s, ld);
